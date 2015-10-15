f6deb66979ec76881c92fc953357e924711ae004
Alan Li <alanli518@gmail.com>
2015-10-14 22:16:01 -0400

    deleted comments in code and added feature to output time

--- Visualization/src/com/alan/Data.java
+++ Visualization/src/com/alan/Data.java
@@ -4,23 +4,31 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Data {
-    //        ********************************************
+    /**
+     Data points starts at 11:26am on 10-04-2015 and ends at 7:18pm on 10-05-2015
+     11:26am is 1443972398000 in UTC milliseconds
+     360 is max number of cars available at any given time
+     382 is number of data points, in 5 min intervals between 11:26am and 7:18pm
+     */
+
     private double[][] xPts = new double[360][382];
     private double[][] yPts = new double[360][382];
     private int loop;
     private String line;
-    private String stringArray;
+    private long timeInMS;
+
 
     public Data() {
         this.loop = 0;
+        this.timeInMS = 1443972398000L;
         try {
-            FileReader fileReader = new FileReader("/Users/alanli/Desktop/Car2Go/Visualization/res/xArray.txt");
+            FileReader fileReader = new FileReader("/Users/alanli/Documents/Car2Go-Project/Visualization/res/xArray.txt");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-//            while ((line = bufferedReader.readLine()) != null) {
-//                stringArray += line;
-//            }
+
             for(int a = 0; a < 382; a++){
                 for(int b = 0; b < 360; b++){
                     try {
@@ -33,11 +41,9 @@ public class Data {
                 }
             }
 
-            fileReader = new FileReader("/Users/alanli/Desktop/Car2Go/Visualization/res/yArray.txt");
+            fileReader = new FileReader("/Users/alanli/Documents/Car2Go-Project/Visualization/res/yArray.txt");
             bufferedReader = new BufferedReader(fileReader);
-//            while ((line = bufferedReader.readLine()) != null) {
-//                stringArray += line;
-//            }
+
             for(int a = 0; a < 382; a++){
                 for(int b = 0; b < 360; b++){
                     try {
@@ -63,15 +69,31 @@ public class Data {
         return yPts;
     }
 
+    public long getTimeInMS() {
+        return timeInMS;
+    }
+
+    public void incTimeInMS() {
+        this.timeInMS += 300000;
+    }
+
     public int getLoop() {
         return loop;
     }
 
     public void incLoop() {
+        System.out.println(getDate(getTimeInMS(), "dd/MM/yyyy hh:mm"));
+        incTimeInMS();
         this.loop += 1;
     }
 
+    public static String getDate(long milliSeconds, String dateFormat)
+    {
+        SimpleDateFormat formatter = new SimpleDateFormat(dateFormat);
 
-    //        ********************************************
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(milliSeconds);
+        return formatter.format(calendar.getTime());
+    }
 
 }
--- Visualization/src/com/alan/DisplayImageEx.java
+++ Visualization/src/com/alan/DisplayImageEx.java
@@ -5,12 +5,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.Random;
 import javax.swing.*;
 
 class Surface extends JPanel implements ActionListener{
 
-    private Image mshi;
+    private Image bgImage;
     private final int DELAY = 1000;
     private Timer timer;
     Data xy = new Data();
@@ -35,26 +34,30 @@ class Surface extends JPanel implements ActionListener{
 
     private void loadImage() {
 
-        mshi = new ImageIcon("/Users/alanli/Desktop/Car2Go/Visualization/res/1200800.png").getImage();
+        bgImage = new ImageIcon("/Users/alanli/Documents/Car2Go-Project/Visualization/res/1200800.png").getImage();
     }
 
     private void setSurfaceSize() {
 
         Dimension d = new Dimension();
-        d.width = mshi.getWidth(null);
-        d.height = mshi.getHeight(null);
+        d.width = bgImage.getWidth(null);
+        d.height = bgImage.getHeight(null);
         setPreferredSize(d);
     }
 
     private void doDrawing(Graphics g, Data xy) {
 
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawImage(mshi, 0, 0, null);
-        g2d.setColor(Color.blue);
+        g2d.drawImage(bgImage, 0, 0, null);
+        g2d.setColor(Color.BLUE);
 
         double xPts[][] = xy.getxPts();
         double yPts[][] = xy.getyPts();
         int loop = xy.getLoop();
+        /**
+         * below is the formula for mapping GPS points to the background map image
+         * using the 4 corners of the map image as reference points
+         */
         for (int z = 0; z < 360; z++) {
             int x = (int)((yPts[z][loop]+79.511267)/.224217*1200);
             int y = (int)(800-((xPts[z][loop]-43.626395)/.10541*800));
@@ -64,21 +67,6 @@ class Surface extends JPanel implements ActionListener{
         if(xy.getLoop() > 381){
             System.exit(0);
         }
-
-
-
-//        int w = getWidth();
-//        int h = getHeight();
-//
-//        Random r = new Random();
-//
-//        for (int i = 0; i < 1000; i++) {
-//
-//            int x = Math.abs(r.nextInt()) % w;
-//            int y = Math.abs(r.nextInt()) % h;
-//            g2d.fillOval(x, y, 5, 5);
-//
-//        }
 
     }
 
